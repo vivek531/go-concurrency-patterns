25b54b202e6d83121fbafaea41f09eed92aae33c
Vivek Kumar <vivekkumar531@gmail.com>
2023-04-21 20:23:11 +0000

    pipeline pattern

---
+++ sync_pipeline/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func sliceToChannel(nums []int) <-chan int {
+	outChannel := make(chan int)
+	go func() {
+		for _, n := range nums {
+			outChannel <- n
+		}
+		close(outChannel)
+	}()
+	return outChannel
+}
+
+func sq(in <-chan int) <-chan int {
+	outChannel := make(chan int)
+	go func() {
+		for n := range in {
+			outChannel <- n * n
+		}
+		close(outChannel)
+	}()
+	return outChannel
+}
+
+func main() {
+	nums := []int{2, 3, 4, 5}
+	inputChannel := sliceToChannel(nums)
+	finalChannel := sq(inputChannel)
+	for n := range finalChannel {
+		fmt.Println(n)
+	}
+}
